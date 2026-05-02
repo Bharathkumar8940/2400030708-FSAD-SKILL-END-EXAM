fc71f08f08a302620cd6b6ff221e08a8415ffba5
Bharathkumar8940 <2400030708@kluniversity>
2026-05-02 21:49:37 +0530

    Removed Experiment-1 folder

--- com.klef.fsad.exam/src/main/java/com/klef/fsad/exam/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klef.fsad.exam;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klef.fsad.exam.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
